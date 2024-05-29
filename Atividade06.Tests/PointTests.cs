2827316f5c42424a964f54c40c2b3f10bd82e43a
LobinhoTroll <gabrielteles931@gmail.com>
2024-05-29 15:31:38 -0300

    Adicionando o teste da 6

---
+++ Atividade06.Tests/PointTests.cs
@@ -0,0 +1,30 @@
+﻿using Atividade06;
+using Xunit.Sdk;
+
+namespace Atividade06.Tests
+{
+    public class PointTests
+    {
+        [Fact]
+        public void DistanceTo_returns_correct_distance()
+        {
+            var point = new Point(1, 0);
+            double result = point.DistanceTo(new Point(5,0));
+            Assert.Equal(4, result);
+        }
+        [Fact]
+        public void DistanceTo_returns_correct_distance_whith_negative()
+        {
+            var point = new Point(-1, 0);
+            double result = point.DistanceTo(new Point(5,0));
+            Assert.Equal(6, result);
+        }
+        [Fact]
+        public void DistanceTo_returns_correct_distance_whith_no_exactly_result()
+        {
+            var point = new Point(1, 1);
+            double result = Math.Round(point.DistanceTo(new Point(5,5)),5);
+            Assert.Equal(5.65685, result);
+        }
+    }
+}
